4ae5af04440581bee49b3fcd1d4737912d17e868
Sebastina <sebane1@thavea.com>
2025-04-01 17:00:26 -0500

    Move to using raw nints instead of getting it from ICharacter. They no longer allow access from other threads.

--- MediaGameObject.cs
+++ MediaGameObject.cs
@@ -1,71 +1,102 @@
-﻿using Dalamud.Game.ClientState.Objects.Enums;
+using Dalamud.Game.ClientState.Objects.Enums;
 using Dalamud.Game.ClientState.Objects.Types;
+using InteropGenerator.Runtime;
 using System;
 using System.Numerics;
 using IMediaGameObject = RoleplayingMediaCore.IMediaGameObject;
 
-namespace RoleplayingVoiceDalamudWrapper {
-    public unsafe class MediaGameObject : IMediaGameObject {
+namespace RoleplayingVoiceDalamudWrapper
+{
+    public unsafe class MediaGameObject : IMediaGameObject
+    {
         private Dalamud.Game.ClientState.Objects.Types.IGameObject _gameObject;
         private FFXIVClientStructs.FFXIV.Client.Game.Object.GameObject* _gameObjectPointer;
         private string _name = "";
         private Vector3 _position = new Vector3();
 
-        string IMediaGameObject.Name {
-            get {
-                try {
+        string IMediaGameObject.Name
+        {
+            get
+            {
+                try
+                {
                     return _gameObjectPointer != null ? (_gameObjectPointer != null ? _gameObjectPointer->NameString : _name) : _name;
-                } catch {
+                }
+                catch
+                {
                     return _name;
                 }
             }
         }
 
-        Vector3 IMediaGameObject.Position {
-            get {
-                try {
+        Vector3 IMediaGameObject.Position
+        {
+            get
+            {
+                try
+                {
                     return (_gameObjectPointer != null ? _gameObjectPointer->Position : _position);
-                } catch {
+                }
+                catch
+                {
                     return _position;
                 }
             }
         }
 
-        Vector3 IMediaGameObject.Rotation {
-            get {
-                try {
+        Vector3 IMediaGameObject.Rotation
+        {
+            get
+            {
+                try
+                {
                     return new Vector3(0, _gameObjectPointer != null ? _gameObjectPointer->Rotation : 0, 0);
-                } catch {
+                }
+                catch
+                {
                     return new Vector3(0, 0, 0);
                 }
             }
         }
 
-        string IMediaGameObject.FocusedPlayerObject {
-            get {
-                if (_gameObject != null) {
-                    try {
-                        return _gameObject.TargetObject != null ?
-                            (_gameObject.TargetObject.ObjectKind == ObjectKind.Player ? _gameObject.TargetObject.Name.TextValue : "")
-                            : "";
-                    } catch {
-                        return "";
-                    }
-                } else {
-                    return "";
-                }
+        // To do: re-implement target detection
+        //string IMediaGameObject.FocusedPlayerObject {
+        //    get {
+        //        if (_gameObject != null) {
+        //            try {
+        //                return _gameObject.TargetObject != null ?
+        //                    (_gameObject.TargetObject.ObjectKind == ObjectKind.Player ? _gameObject.TargetObject.Name.TextValue : "")
+        //                    : "";
+        //            } catch {
+        //                return "";
+        //            }
+        //        } else {
+        //            return "";
+        //        }
+        //    }
+        //}
+
+        string IMediaGameObject.FocusedPlayerObject
+        {
+            get
+            {
+                return "";
             }
         }
 
-        Vector3 IMediaGameObject.Forward {
-            get {
+        Vector3 IMediaGameObject.Forward
+        {
+            get
+            {
                 float rotation = _gameObjectPointer != null ? _gameObjectPointer->Rotation : 0;
                 return new Vector3((float)Math.Cos(rotation), 0, (float)Math.Sin(rotation));
             }
         }
 
-        public Vector3 Top {
-            get {
+        public Vector3 Top
+        {
+            get
+            {
                 return new Vector3(0, 1, 0);
             }
         }
@@ -74,22 +105,22 @@ namespace RoleplayingVoiceDalamudWrapper {
 
         bool IMediaGameObject.Invalid => false;
 
-        public MediaGameObject(Dalamud.Game.ClientState.Objects.Types.IGameObject gameObject) {
-            if (gameObject != null) {
-                _gameObject = gameObject;
-                _gameObjectPointer = (FFXIVClientStructs.FFXIV.Client.Game.Object.GameObject*)(gameObject as ICharacter).Address;
-            }
+        public MediaGameObject(nint address)
+        {
+            _gameObjectPointer = (FFXIVClientStructs.FFXIV.Client.Game.Object.GameObject*)address;
         }
-        unsafe public MediaGameObject(FFXIVClientStructs.FFXIV.Client.Game.Object.GameObject* gameObject) {
+        unsafe public MediaGameObject(FFXIVClientStructs.FFXIV.Client.Game.Object.GameObject* gameObject)
+        {
             _gameObjectPointer = gameObject;
         }
-        public MediaGameObject(string name, Vector3 position) {
+        public MediaGameObject(string name, Vector3 position)
+        {
             _name = name;
             _position = position;
         }
-        public MediaGameObject(Dalamud.Game.ClientState.Objects.Types.IGameObject gameObject, string name, Vector3 position) {
-            _gameObject = gameObject;
-            _gameObjectPointer = (FFXIVClientStructs.FFXIV.Client.Game.Object.GameObject*)(gameObject as ICharacter).Address;
+        public MediaGameObject(nint address, string name, Vector3 position)
+        {
+            _gameObjectPointer = (FFXIVClientStructs.FFXIV.Client.Game.Object.GameObject*)address;
             _name = name;
             _position = position;
         }
